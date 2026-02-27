6056506c95b89341174864b601c0c13171a3a926
Prasham Shah <95548088+ps-coding@users.noreply.github.com>
2026-02-27 17:44:34 -0500

    208-are we on budget

--- CodeQuest/208-are_we_on_budget.py
+++ CodeQuest/208-are_we_on_budget.py
@@ -1,19 +1,16 @@
 import sys
 
-import math
-
-
-def normal_round(n):
-    if n - math.floor(n) < 0.5:
-        return math.floor(n)
-    return math.ceil(n)
+from decimal import Decimal, ROUND_HALF_UP
 
 
 def solve(budgetedCosts, actualCosts, numItems):
-    sum = 0
+    if numItems == 0:
+        return Decimal(0)
+
+    sum = Decimal(0)
     for i in range(numItems):
-        sum += float(actualCosts[i]) - float(budgetedCosts[i])
-    return sum / numItems
+        sum += Decimal(actualCosts[i]) - Decimal(budgetedCosts[i])
+    return sum / Decimal(numItems)
 
 
 numCases = int(sys.stdin.readline().rstrip())
@@ -21,4 +18,6 @@ for caseNum in range(numCases):
     numItems = int(sys.stdin.readline().rstrip())
     budgetedCosts = sys.stdin.readline().rstrip().split(" ")
     actualCosts = sys.stdin.readline().rstrip().split(" ")
-    print(f"{(normal_round(solve(budgetedCosts, actualCosts, numItems))):.2f}")
+    print(
+        f"{solve(budgetedCosts, actualCosts, numItems).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)}"
+    )
